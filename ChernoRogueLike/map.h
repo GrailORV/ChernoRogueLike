5fa7a6654f84e7f4e8979f580e9e74b133129f3f
geskysan <sakuya3983@ezweb.ne.jp>
2018-03-27 14:23:17 +0900

    プルしたいからとりあえず更新

--- ChernoRogueLike/map.h
+++ ChernoRogueLike/map.h
@@ -6,7 +6,7 @@
 //=============================================================================
 #pragma once
 #include "main.h"
-#include "scene.h"
+#include "scene2D.h"
 
 //*****************************************************************************
 // マクロ定義
@@ -21,11 +21,32 @@
 //*****************************************************************************
 // マップクラス
 //*****************************************************************************
-class CMap : public CScene
+class CMap : public CScene2D
 {
 public:
 	CMap();
 	~CMap();
 
-	static CMap *Create(int nType,)
+	static CMap *Create(int nType, D3DXVECTOR3 pos);
+
+	HRESULT Init(D3DXVECTOR3 pos, D3DXVECTOR3 rot);
+	HRESULT Init(void) { return S_OK; }
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	void SetPosition(D3DXVECTOR3 pos) { m_pos = pos; }
+	D3DXVECTOR3 GetPosition(void) { return m_pos; }
+
+	void SetRotation(D3DXVECTOR3 rot) { m_rot = rot; }
+	D3DXVECTOR3 GetRotation(void) { return m_rot; }
+
+	typedef struct
+	{
+
+	};
+
+private:
+	//D3DXVECTOR3 m_pos;
+	//D3DXVECTOR3 m_rot;
 };
